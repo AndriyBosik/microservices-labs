5ad2250ee7aafd70c72167fd59387533cb7b08a9
AndriyBosik <andriybosik2002@ukr.net>
2021-10-10 20:02:14 +0300

    Client side was implemented for lab2

---
+++ lab1/client/src/App.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const [message, setMessage] = useState(null);
+  
+    useEffect(() => {
+        const fetchMessage = async () => {
+            const response = await axios.get("http://localhost:8080/api/messages/simple");
+            setMessage(response.data.text);
+        }
+
+        fetchMessage();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                {
+                message == null ? (<p>Fetching message...</p>) : (<p>Message received: {message}</p>)
+                }
+            </header>
+        </div>
+    );
+}
+
+export default App;
